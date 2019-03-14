32e706ee318a6b2592d54098ee98ff98e0b5b945
InterLink Intern <intern@intern-pc.interlink.office>
2019-03-14 09:04:24 +0200

    Add build.js

---
+++ build.js
@@ -0,0 +1,12 @@
+const opn = require('opn');
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running');
+    opn('index.html');
+})
